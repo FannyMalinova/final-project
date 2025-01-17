83a90b414e5f2e95bb0fdf7a26e2a4f47e678661
Fanny Malinova <fannymalinova@yahoo.com>
2025-01-17 14:09:10 +0200

    Adding the initial setup for Terraform to connect to AWS and write its state file into an S3 bucket and a lock in a DynamoDB table.

---
+++ terraform/setup/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.83.1"
+
+    }
+  }
+
+  backend "s3" {
+    bucket         = "final-project-s3"
+    key            = "tf-state-config"
+    region         = "eu-north-1"
+    encrypt        = true
+    dynamodb_table = "final-project-dynamo-table"
+  }
+  
+}
+
+provider "aws" {
+  
+  region = var.region
+  default_tags {
+    tags = {
+      Environment = terraform.workspace
+      Project     = var.project
+      contact     = var.contact
+      ManageBy    = "Terraform/setup"
+    }
+  }
+}
---
+++ terraform/setup/variables.tf
@@ -0,0 +1,29 @@
+variable "tf-state-bucket" {
+  description = "S3 bucket for storing Terraform state"
+  default     = "final-project-s3"
+}
+
+variable "tf-state-key" {
+  description = "S3 bucket key"
+  default     = "tf-state-config"
+}
+
+variable "tf-state-lock-table" {
+  description = "DynamoDB table for storing Terraform state file lock"
+  default     = "final-project-dynamo-table"
+}
+
+variable "region" {
+  description = "Default AWS region"
+  default     = "eu-north-1"
+}
+
+variable "project" {
+  description = "Project name for resource tagging"
+  default     = "budget-app"
+}
+
+variable "contact" {
+  description = "Contact name for resource tagging"
+  default     = "fannymalinova@yahoo.com"
+}
