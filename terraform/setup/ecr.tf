01757729c4d9733fbb4f517bca59d2db43f08a53
Fanny Malinova <fannymalinova@yahoo.com>
2025-01-17 16:28:18 +0200

    Added creation of ECR repository in AWS.

---
+++ terraform/setup/ecr.tf
@@ -0,0 +1,14 @@
+###################
+# Create ESR repository for storing Docker images in AWS.
+###################
+
+resource "aws_ecr_repository" "budget-app-repo" {
+  name                 = "budget-app-repo"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
--- terraform/setup/outputs.tf
+++ terraform/setup/outputs.tf
@@ -1,3 +1,7 @@
+####################
+# Output values for budget-user AWS login credentials.
+####################
+
 output "budget-user-access-key-id" {
   description = "AWS key ID for the budget-user"
   value       = aws_iam_access_key.budget-user.id
@@ -7,4 +11,13 @@ output "budget-user-access-key-secret" {
   description = "Access secret for the budget-user"
   value       = aws_iam_access_key.budget-user.secret
   sensitive   = true
+}
+
+#####################
+# Output URLs for the ECR repository.
+#####################
+
+output "ecr-repo-budget-app" {
+  description = "URL of the ECR repository for the budget app container image"
+  value       = aws_ecr_repository.budget-app-repo.repository_url
 }
