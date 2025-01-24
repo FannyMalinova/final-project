6460fb7f6be40468bcfd22f4026c2615788d68a3
Fanny Malinova <fannymalinova@yahoo.com>
2025-01-24 21:23:14 +0200

    Adding security groups

--- terraform/deploy/database.tf
+++ terraform/deploy/database.tf
@@ -19,11 +19,16 @@ resource "aws_security_group" "rds" {
     from_port = 5432
     to_port   = 5432
     protocol  = "tcp"
+
+  security_groups = [
+      aws_security_group.ecs-service.id
+    ]
   }
 
   tags = {
     Name = "${local.prefix}-db-security-group"
   }
+
 }
 
 resource "aws_db_instance" "db-main" {
